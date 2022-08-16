9cfd7cf72f9f17c08607faff8c32a4f9c9c0bb87
fan9704 <cxz123499@gmail.com>
2022-08-17 01:33:00 +0800

    Reconstruct main.dart and complete theme.dart for change theme and delete unused component linechart.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:ecg/Page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'Page/home_page.dart';
+import 'Page/heartRateInfo/heartRateInfoPage.dart';
+import 'Page/singleDayAnalysis/singleDayAnalysisPage.dart';
 import 'config/palette.dart';
 void main() => runApp(const MyApp());
 
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Palette.mycolor,
       ),
-      home: Scaffold(
+      home: const Scaffold(
         body: BottomNavigationController(),
 
       ),
@@ -28,8 +30,11 @@ class BottomNavigationController extends StatefulWidget {
       _BottomNavigationControllerState();
 }
 class _BottomNavigationControllerState extends State<BottomNavigationController> {
-  //int _currentIndex = 0; //預設值
-  final pages = [GroupWidget()];
+  int _currentIndex = 0;
+  final pages = [const HomePage() ,const HeartRateInfoPage(),const SingleDayAnalysisPage()];
+  final _controller = PageController(initialPage: 0);
+  static const _duration = Duration(milliseconds: 300);
+  static const _curve = Curves.easeInOutCubic;
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -39,57 +44,48 @@ class _BottomNavigationControllerState extends State<BottomNavigationController>
           Scaffold(appBar: AppBar(
             title: const Text(''),
           ),
-          body:
-            GroupWidget(),
-          )
-          ,BottomNavigationBar(
-            selectedLabelStyle: const TextStyle(fontSize: 12),
-            unselectedLabelStyle: const TextStyle(fontSize: 12),
-            unselectedItemColor: Colors.white,
-            selectedItemColor: Colors.white,
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: const Color(0xFF709e94),
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: FaIcon(
-                  FontAwesomeIcons.heartPulse,
-                  color: Colors.white,
+            body: pages[_currentIndex],
+            // TODO: Detect is login or register not to show
+            bottomNavigationBar: BottomNavigationBar(
+              selectedLabelStyle: const TextStyle(fontSize: 12),
+              unselectedLabelStyle: const TextStyle(fontSize: 12),
+              unselectedItemColor: Colors.white,
+              selectedItemColor: Colors.white,
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: const Color(0xFF709e94),
+              onTap: _onItemClick,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: FaIcon(
+                    FontAwesomeIcons.heartPulse,
+                    color: Colors.white,
+                  ),
+                  label: '每日',
                 ),
-                label: '每日',
-              ),
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.chartSimple, color: Colors.white),
-                label: '頻率',
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(FontAwesomeIcons.house, color: Colors.transparent),
-                  label: ""),
-              BottomNavigationBarItem(
-                icon: FaIcon(FontAwesomeIcons.gear, color: Colors.white),
-                label: '設定',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.help, color: Colors.white),
-                label: '說明',
-              ),
-            ],
-          ),
-          Positioned(
-            top: 700.0,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: const Color(0xFFe8998d),
-                shape: const CircleBorder(),
-                padding: const EdgeInsets.all(15),
-              ),
-              onPressed: () => null,
-              child: const FaIcon(
-                FontAwesomeIcons.house,
-                size: 30,
-                color: Colors.white,
-              ),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.chartSimple, color: Colors.white),
+                  label: '頻率',
+                ),
+                BottomNavigationBarItem(
+                    icon: Icon(FontAwesomeIcons.house, color: Colors.transparent),
+                    label: ""),
+                BottomNavigationBarItem(
+                  icon: FaIcon(FontAwesomeIcons.gear, color: Colors.white),
+                  label: '設定',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.help, color: Colors.white),
+                  label: '說明',
+                ),
+              ],
             ),
+
           )
         ]);
+  }
+  void _onItemClick(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
